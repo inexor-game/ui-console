44a53f7a4bf04235bc3a3692917fce5492292e9f
Andreas Schaeffer <hanack@nooblounge.net>
2017-07-08 12:41:43 +0200

    Make the console able to handle other ports

--- src/InexorConsole.js
+++ src/InexorConsole.js
@@ -7,12 +7,31 @@ export default class InexorConsole {
 
   constructor() {
 
+    /**
+     * Get the URL parameters.
+     * 
+     * instanceId: The id of the Inexor Core instance.
+     * host: The hostname of the Inexor Flex instance.
+     * port: The port of the Inexor Flex instance.
+     */
+    this.parameters = this.getUrlParameters();
+
+    /**
+     * Create an empty tree.
+     */
     this.root = new tree.Root();
-    this.treeWebsocket = new WebSocket("ws://localhost:31416/api/v1/ws/tree");
+
+    /**
+     * Open websocket for the Inexor Tree.
+     */
+    this.treeWebsocket = new WebSocket(util.format('ws://%s:%s/api/v1/ws/tree', this.parameters.host, this.parameters.port));
     // TODO: handle updates on the console configuration (size, colors, ...)
     // this.treeWebsocket.onmessage = this.onTreeMessage.bind(this);
 
-    this.consoleWebsocket = new WebSocket("ws://localhost:31416/api/v1/ws/console");
+    /**
+     * Open websocket for the console.
+     */
+    this.consoleWebsocket = new WebSocket(util.format('ws://%s:%s/api/v1/ws/console', this.parameters.host, this.parameters.port));
     this.consoleWebsocket.onmessage = this.onConsoleWsMessage.bind(this);
     this.consoleWebsocket.onopen = this.onConsoleWsOpen.bind(this);
     this.consoleWebsocket.onerror = this.onConsoleWsError.bind(this);
@@ -20,9 +39,6 @@ export default class InexorConsole {
     // Contains the console instances
     this.consoleInstances = {};
 
-    // The instance id
-    this.instanceId = this.getInstanceId();
-    console.log('Instance ID: ' + this.instanceId);
   }
 
   createConsole(instanceId) {
@@ -62,7 +78,7 @@ export default class InexorConsole {
   }
 
   onConsoleWsOpen() {
-    this.createConsole(this.instanceId);
+    this.createConsole(this.parameters.instanceId);
   }
 
   onConsoleWsError(error) {
@@ -149,22 +165,26 @@ export default class InexorConsole {
   }
 
   /**
-   * Extracts the instance id from the URL.
+   * Extracts the URL parameters.
    * @function
-   * @name getInstanceId
-   * @returns {string} The instance id.
+   * @name getParameters
+   * @returns {object} The parameters.
    */
-  getInstanceId() {
+  getUrlParameters() {
+    let parameters = {
+      'host': 'localhost',
+      'port': 31416,
+      'instanceId': null
+    };
     let query = window.location.search.split('?');
     if (query.length == 2) {
       let params = query[1].split('&');
       for (var i = 0; i < params.length; i++) {
-        if (params[i].split('=')[0] == 'instanceId') {
-          return params[i].split('=')[1];
-        }
+        let kv = params[i].split('=');
+        parameters[kv[0]] = kv[1];
       }
     }
-    return '';
+    return parameters;
   }
 
 }
